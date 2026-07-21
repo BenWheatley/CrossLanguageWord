bc2cce726a72a653765f35ca55dd293b17445ae9
Ben Wheatley <BenWheatley@users.noreply.github.com>
2026-07-22 00:20:10 +0200

    better

--- url.js
+++
@@ -1,7 +0,0 @@
-export function parseURLParams() {
-  const p = new URLSearchParams(window.location.search);
-  return {
-    words: p.get("words") ? parseInt(p.get("words"), 10) : null,
-    wordList: p.get("wordList")
-  };
-}
